56f52cc63a72c9dc5247641c8c45d174424e017e
JJ-in-Jersey <118015245+JJ-in-Jersey@users.noreply.github.com>
2023-01-18 16:28:52 -0500

    can't reference read_df, trying classmethod

--- install.py
+++ install.py
@@ -13,17 +13,16 @@ if __name__ == '__main__':
     while None in source_packages: source_packages.remove(None)
 
     destination = Path(site.USER_SITE)
-    destination_packages = [dir for dir in os.listdir(destination)]
+    if destination.exists():
+        destination_packages = [dir for dir in os.listdir(destination)]
 
-    for package in destination_packages:
-        print(f'Delete package {package} from {destination}')
-        shutil.rmtree(os.path.join(destination, package))
+        for package in destination_packages:
+            print(f'Delete package {package} from {destination}')
+            shutil.rmtree(os.path.join(destination, package))
 
     for package in source_packages:
         print(f'Copying package {package} from {source} to {destination}')
         shutil.copytree(os.path.join(source,package), os.path.join(destination,package))
-
-
 
 
 
