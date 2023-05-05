ef1bcc66b654a14d536d17ac37d9fb199b7b7b68
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-05-05 18:57:42 -0400

    new commit

--- GPX/__init__.py
+++ GPX/__init__.py
@@ -1,13 +1,17 @@
 from bs4 import BeautifulSoup as Soup
 from Navigation import Navigation as NV
+from pathlib import Path
+from os import makedirs
 
 class Waypoint:
 
-    type = {'CurrentStationWP': 'Symbol-Spot-Orange', 'PlaceHolderWP': 'Symbol-Spot-Green', 'InterpolationWP': 'Symbol-Spot-Blue', 'InterpolationDataWP': 'Symbol-Spot-Black'}
+    velocity_folder = None
+    type = {'CurrentStationWP': 'Symbol-Spot-Orange', 'LocationWP': 'Symbol-Spot-Green', 'InterpolationWP': 'Symbol-Spot-Blue', 'DataWP': 'Symbol-Spot-Black'}
     ordinal_number = 0
     index_lookup = {}
 
     def __init__(self, gpxtag):
+
         self.lat = round(float(gpxtag.attrs['lat']), 4)
         self.lon = round(float(gpxtag.attrs['lon']), 4)
         self.index = Waypoint.ordinal_number
@@ -18,10 +22,22 @@ class Waypoint:
         self.prev_edge = None
         self.next_edge = None
 
+        if Waypoint.velocity_folder is None: raise TypeError
+        makedirs(Waypoint.velocity_folder.joinpath(self.short_name), exist_ok=True)
+        self.velo_file = Waypoint.velocity_folder.joinpath(self.short_name + '_array')
+
         Waypoint.index_lookup[Waypoint.ordinal_number] = self
         Waypoint.ordinal_number += 1
 
-class PlaceHolderWP(Waypoint):
+class PseudoWP(Waypoint):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+class ArtificialWP(PseudoWP):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+class LocationWP(PseudoWP):
     def __init__(self, gpxtag):
         super().__init__(gpxtag)
 
@@ -37,40 +53,45 @@ class CurrentStationWP(Waypoint):
         self.noaa_code = gpxtag.find('link').find('text').text
         self.velo_arr = None
 
-class InterpolationDataWP(Waypoint):
+class DataWP(ArtificialWP):
     def __init__(self, gpxtag):
         super().__init__(gpxtag)
         self.noaa_url = gpxtag.find('link').attrs['href'] if gpxtag.link else None
         self.noaa_code = gpxtag.find('link').find('text').text
-        self.download_velo_arr = None
+        self.velo_arr = None
 
 class Edge:
+
+    elapsed_time_folder = None
+
     def __init__(self, start, end):
         if start == end: raise IndexError
+        self.name = '[' + str(start.index) + '-' + str(end.index) + ']'
         self.start = start
         self.end = end
-        self.name = '[' + str(self.start.index) + '-' + str(self.end.index) + ']'
+        self.et_df = None
         start.next_edge = self
         end.prev_edge = self
 
-class HaversineEdge(Edge):
+        if Edge.elapsed_time_folder is None: raise TypeError
+        makedirs(Edge.elapsed_time_folder.joinpath(self.name), exist_ok=True)
+        self.et_file = Edge.elapsed_time_folder.joinpath(self.name + '_array')
+
+class SimpleEdge(Edge):
     def __init__(self, start, end):
         super().__init__(start, end)
-        self.dataframe = None
         self.length = round(NV.distance(self.start.coords, self.end.coords), 4)
 
-class LengthSumEdge(Edge):
+class CompositeEdge(Edge):
     def __init__(self, start, end):
         super().__init__(start, end)
-        self.dataframe = None
         self.length = 0
-        wp_range = range(start.index, end.index) if start.index < end.index else range(end.index, start.index)
-        for i in wp_range:
-            start_coords = Waypoint.index_lookup[i].coords
-            end_coords = Waypoint.index_lookup[i+1].coords
-            self.length += round(NV.distance(start_coords, end_coords), 4)
+        wp_range = range(start.index, end.index+1)
+        waypoints = [Waypoint.index_lookup[i] for i in wp_range if not isinstance(Waypoint.index_lookup[i], ArtificialWP)]
+        for i, wp in enumerate(waypoints[:-1]):
+            self.length += round(NV.distance(wp.coords, waypoints[i+1].coords), 4)
 
-class Path:
+class GPXPath:
 
     def path_integrity(self):
         for i, edge in enumerate(self.edges[:-1]):
@@ -107,10 +128,8 @@ class Route:
         self.name = filepath.stem
         self.__transit_time_dict = {}
         self.__elapsed_time_dict = {}
-        self.whole_path = None
-        self.velo_path = None
+        self.whole_path = self.velo_path = None
         self.interpolation_groups = []
-        self.current_stations = None
 
         with open(filepath, 'r') as f: gpxfile = f.read()
         tree = Soup(gpxfile, 'xml')
@@ -119,33 +138,30 @@ class Route:
         waypoints = []
         for waypoint in tree.find_all('rtept'):
             if waypoint.sym.text == Waypoint.type['CurrentStationWP']: waypoints.append(CurrentStationWP(waypoint))
-            elif waypoint.sym.text == Waypoint.type['PlaceHolderWP']: waypoints.append(PlaceHolderWP(waypoint))
+            elif waypoint.sym.text == Waypoint.type['LocationWP']: waypoints.append(LocationWP(waypoint))
             elif waypoint.sym.text == Waypoint.type['InterpolationWP']: waypoints.append(InterpolationWP(waypoint))
-            elif waypoint.sym.text == Waypoint.type['InterpolationDataWP']: waypoints.append(InterpolationDataWP(waypoint))
+            elif waypoint.sym.text == Waypoint.type['DataWP']: waypoints.append(DataWP(waypoint))
 
-        self.current_stations = [wp for wp in waypoints if isinstance(wp, CurrentStationWP)]
+        # create all the edges and the whole path
+        edges = [SimpleEdge(wp, Waypoint.index_lookup[wp.index+1]) for wp in waypoints[:-1]]
+        self.whole_path = GPXPath(edges)
 
-        edges = [HaversineEdge(waypoint, waypoints[i+1]) for i, waypoint in enumerate(waypoints[:-1])]
-        self.whole_path = Path(edges)
-
-        #  calculate interpolation groups
+        # create interpolation groups
         wp_len = len(waypoints)
         for i in range(0, wp_len):
             if isinstance(waypoints[i], InterpolationWP):
-                group = [waypoints[i]] + [waypoints[j] for j in range(i+1, wp_len) if isinstance(waypoints[j], InterpolationDataWP)]
+                group = [waypoints[i]] + [waypoints[j] for j in range(i+1, wp_len) if isinstance(waypoints[j], DataWP)]
                 self.interpolation_groups.append(group)
 
-        # velo_wps = [wp for wp in waypoints if type(wp) == CurrentStationWP or type(wp) == InterpolationWP]
-        velo_wps = [wp for wp in waypoints if type(wp) == CurrentStationWP]
+        # create velocity path
+        velo_wps = [wp for wp in waypoints if not isinstance(wp, PseudoWP)]
         velo_edges = []
         for i, wp in enumerate(velo_wps[:-1]):
-            if wp.index+1 == velo_wps[i+1].index:  # adjacent points
-                velo_edges.append(HaversineEdge(wp, velo_wps[i+1]))
-            elif type(wp) == InterpolationWP or type(velo_wps[i+1]) == InterpolationWP:  # skip data points
-                velo_edges.append(HaversineEdge(wp, velo_wps[i + 1]))
-            else:  # add placeholder points
-                velo_edges.append(LengthSumEdge(wp, velo_wps[i + 1]))
-        self.velo_path = Path(velo_edges)
+            if wp.index+1 == velo_wps[i+1]:
+                velo_edges.append(SimpleEdge(wp, velo_wps[i+1]))
+            else:
+                velo_edges.append(CompositeEdge(wp, velo_wps[i + 1]))
+        self.velo_path = GPXPath(velo_edges)
 
     def print_route(self):
         print(f'\n{self.name}')
