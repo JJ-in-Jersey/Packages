446a7826f2668343be98b7006fba9cd5612c2d62
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-05-08 08:35:46 -0400

    new commit

--- FileTools/__init__.py
+++ FileTools/__init__.py
@@ -1,7 +1,5 @@
 from glob import glob
 from os.path import join, getctime
-from pathlib import Path
-from os import makedirs
 
 class FileTools:
 
@@ -12,12 +10,12 @@ class FileTools:
         for t in types: files.extend(glob(join(folder, t)))
         return max(files, key=getctime) if len(files) else None
 
-    @staticmethod
-    def make_folder(parent, child):
-        if not isinstance(parent, Path): raise TypeError
-        path = parent.joinpath(child)
-        makedirs(path, exist_ok=True)
-        return path
+    # @staticmethod
+    # def make_folder(parent, child):
+    #     if not isinstance(parent, Path): raise TypeError
+    #     path = parent.joinpath(child)
+    #     makedirs(path, exist_ok=True)
+    #     return path
 
     def __init__(self):
         pass
