8841d2fa39cd062fdaf4ba5a45e7fea2414504b2
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-06-11 16:34:06 -0400

    new commit

--- Geometry/__init__.py
+++ Geometry/__init__.py
@@ -13,54 +13,72 @@ class Arc:
     round_to = 15
     name = None
 
-    def angles(self): return pd.Series([self.date(), self.start_angle, self.min_angle, self.end_angle, Arc.name])
-    def times(self): return pd.Series([self.date(), self.start_time, self.min_time, self.end_time, Arc.name])
-
-    def arc_name(self, name):
-        self.name = name
-
     def __init__(self, *args):
         self.tts = args[0]
         self.start_index = args[1]
         self.min_index = args[2]
         self.end_index = args[3]
-        self.name = None
         self.min_time = None
         self.min_angle = None
-        self.date = None
+        self.fractured = False
 
-        self.base_start_time = pd.to_datetime(self.start_index, unit='s').round('min')
-        self.base_end_time = pd.to_datetime(self.end_index, unit='s').round('min')
-        self.base_date = self.base_start_time.date
-
-        if self.min_index is not None:
-            self.base_min_time = pd.to_datetime(self.min_index, unit='s').round('min')
+        self.base_start = pd.to_datetime(self.start_index, unit='s').round('min')
+        self.base_min = pd.to_datetime(self.min_index, unit='s').round('min')
+        self.base_end = pd.to_datetime(self.end_index, unit='s').round('min')
 
 class RoundedArc(Arc):
 
+    def df_angles(self): return pd.Series([self.start_date, self.start_angle, self.min_angle, self.end_angle, Arc.name])
+    def fractional_arc_args(self): return tuple([self.start_date, self.start_angle, self.min_angle, self.end_angle, Arc.name])
+    def print_times(self): print(f's: {self.start_time} m: {self.min_time} e: {self.end_time}')
+    def print_angles(self): print(f's: {self.start_angle} m: {self.min_angle} e: {self.end_angle}')
+
     def __init__(self, *args):
         super().__init__(*args)
-        self.fractured = False
-        self.has_minimum = False
 
-        self.start_time = round_time_to(self.base_start_time, Arc.round_to)
-        self.end_time = round_time_to(self.base_end_time, Arc.round_to)
+        self.start = round_time_to(self.base_start, Arc.round_to)
+        self.min = round_time_to(self.base_min, Arc.round_to)
+        self.end = round_time_to(self.base_end, Arc.round_to)
 
-        if self.start_time.date() != self.end_time.date(): self.fractured = True
-        self.date = self.start_time.date
+        self.start_date = self.start.date()
+        self.start_time = self.start.time()
+        self.end_date = self.end.date()
+        self.end_time = self.end.time()
+        self.min_date = self.min.date()
+        self.min_time = self.min.time()
 
-        self.start_angle = time_to_degrees(self.start_time)
-        self.end_angle = time_to_degrees(self.end_time)
+        self.start_angle = time_to_degrees(self.start)
+        self.end_angle = time_to_degrees(self.end)
+        self.min_angle = time_to_degrees(self.min)
 
-        if self.base_min_time is not None:
-            self.min_time = round_time_to(self.base_min_time, Arc.round_to)
-            self.min_angle = time_to_degrees(self.min_time)
+        if self.start_date != self.end_date and self.end_angle != 0:
+            self.fractured = True
+        else:
+            self.end_angle = 360
+            if self.min_angle == 0: self.min_angle = 360
 
-def fractured_arc_clone(arc: Arc):
-    if not arc.fractured: raise TypeError
-    if arc.min_angle == 0:
-        print('min is zero')
-    elif 360 > arc.min_angle > 180:
-        print('min on left')
-    else:
-        print('min on right')
+class FractionalArc:
+
+    def df_angles(self): return pd.Series([self.start_date, self.start_angle, self.min_angle, self.end_angle, Arc.name])
+
+    # start_date, start_angle, min_angle, end_angle, name
+    def __init__(self, *args):
+        self.start_date = args[0]
+        self.start_angle = args[1]
+        self.min_angle = args[2]
+        self.end_angle = args[3]
+        self.name = args[4]
+
+class FractionalArcStartDay(FractionalArc):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.end_angle = 360
+        if 180 > self.min_angle >= 0: self.min_angle = None
+
+class FractionalArcEndDay(FractionalArc):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.start_angle = 0
+        if 360 > self.min_angle > 180: self.min_angle = None
