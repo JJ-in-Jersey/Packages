4c5cc18f9d55dff32fc07d788062ef57c45fa9c4
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-07-28 19:15:55 -0400

    eliminating chrome driver manager

--- tt_chrome_driver/tt_chrome_driver/chrome_driver.py
+++ tt_chrome_driver/tt_chrome_driver/chrome_driver.py
@@ -3,21 +3,8 @@ from os import environ
 import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 logging.getLogger('WDM').setLevel(logging.NOTSET)
-
-def update_driver():
-    my_options = Options()
-    environ['WDM_LOG'] = "false"
-    environ['WDM_LOG_LEVEL'] = '0'
-    my_options.add_argument("--headless")
-    my_options.add_argument('disable-notifications')
-    my_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=my_options)
-    driver.minimize_window()
-    driver.close()
 
 def get_driver(download_dir=None):
     my_options = Options()
@@ -27,7 +14,8 @@ def get_driver(download_dir=None):
     my_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     if download_dir is not None:
         my_options.add_experimental_option("prefs", {'download.default_directory': str(download_dir)})
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=my_options)
-    driver.implicitly_wait(30)  # seconds
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=my_options)
+    driver = webdriver.Chrome(options=my_options)
+    driver.implicitly_wait(10)  # seconds
     driver.minimize_window()
     return driver
