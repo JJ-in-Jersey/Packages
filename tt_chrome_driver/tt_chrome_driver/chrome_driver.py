232eaad5a048d6c0f460337a3535c9b211c5ce83
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-08-19 20:05:20 -0400

    adding chrome version tools

--- tt_chrome_driver/tt_chrome_driver/chrome_driver.py
+++ tt_chrome_driver/tt_chrome_driver/chrome_driver.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from os import listdir
+import re
 import platform
 import chromedriver_autoinstaller
 
@@ -16,7 +18,8 @@ logger.addHandler(handler)
 
 def get_driver(download_dir=None):
     print(f'chrome version: {chromedriver_autoinstaller.get_chrome_version()}')
-    print(f'isChromeInstalled: {is_chrome_installed()}')
+    print(f'is_chrome_installed: {is_chrome_installed()}')
+    get_installed_chrome_version()
     chromedriver_autoinstaller.install()
     my_options = Options()
     if download_dir is not None:
@@ -28,11 +31,23 @@ def get_driver(download_dir=None):
 
 
 def is_chrome_installed():
-    apple_path = Path('/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
-    windows_path = Path('C:/Program Files/Google/Chrome/Application/Chrome.exe')
+    apple_exe_path = Path('/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
+    windows_exe_path = Path('C:/Program Files/Google/Chrome/Application/Chrome.exe')
 
     if platform.system() == 'Darwin':
-        return apple_path.exists()
+        return apple_exe_path.exists()
     elif platform.system() == 'Windows':
-        return windows_path.exists()
-    pass
+        return windows_exe_path.exists()
+
+
+def get_installed_chrome_version():
+    apple_version_path = Path('/Applications/Google Chrome.app/Contents/Frameworks/Google Chrome Framework.framework/Versions')
+    windows_version_path = Path('C:/Program Files/Google/Chrome/Application/Chrome.exe')
+
+    regex_pattern = '^[0-9\.]*$'
+
+    if platform.system() == 'Darwin':
+        file_list = [re.search(pattern, s) for s in listdir(apple_version_path)]
+    elif platform.system() == 'Windows':
+        file_list = [re.search(pattern, s) for s in listdir(windows_version_path)]
+
