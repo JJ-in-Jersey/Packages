8ecd160e4c7fcd011cf687b8956b4c6aee88d22d
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-08-27 10:14:15 -0400

    fixing Apple version

--- tt_os_abstraction/tt_os_abstraction/os_abstraction.py
+++ tt_os_abstraction/tt_os_abstraction/os_abstraction.py
@@ -1,6 +1,12 @@
 from os import environ
 import platform
 
+platform_lookup = { 'darwin': {'user_profile': 'HOME', 'temp': 'TMPDIR'}, 'windows': {'user_profile': 'USERPROFILE', 'temp': 'TEMP'}}
+
+
+def env(var_name):
+    return platform_lookup[platform.system()][var_name]
+
 
 def user_profile():
     if platform.system() == 'Darwin':
