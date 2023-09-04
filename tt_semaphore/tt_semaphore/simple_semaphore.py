9d806f0808b5d849ad0447c617dd9333a74ee4c8
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-09-03 21:23:16 -0400

    fixing Apple version

--- tt_semaphore/tt_semaphore/simple_semaphore.py
+++ tt_semaphore/tt_semaphore/simple_semaphore.py
@@ -3,8 +3,8 @@ from os import remove
 
 from tt_os_abstraction.os_abstraction import env
 
-def on(name): Path(temp()).joinpath(name).with_suffix('.tmp').touch()
+def on(name): Path(env('temp')).joinpath(name).with_suffix('.tmp').touch()
 
-def off(name): remove(Path(temp()).joinpath(name).with_suffix('.tmp'))
+def off(name): remove(Path(env('temp')).joinpath(name).with_suffix('.tmp'))
 
-def is_on(name): return True if Path(temp()).joinpath(name).with_suffix('.tmp').exists() else False
+def is_on(name): return True if Path(env('temp')).joinpath(name).with_suffix('.tmp').exists() else False
