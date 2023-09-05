71a520f39696d521e6b94fec8da5eb913af34b2d
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-09-05 14:45:52 -0400

    scipy dependency

--- tt_interpolation/setup.py
+++ tt_interpolation/setup.py
@@ -4,5 +4,5 @@ setup(
     name='tt_interpolation',
     version='1.0',
     packages=find_packages(include=['tt_interpolation', 'tt_interpolation.*']),
-    install_requires=['numpy', 'sympy', 'matplotlib']
+    install_requires=['scipy', 'numpy', 'sympy', 'matplotlib']
 )
