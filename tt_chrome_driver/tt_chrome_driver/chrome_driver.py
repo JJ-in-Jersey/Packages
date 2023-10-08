61365d46c831c3cacecc1331ad8958b33450ad38
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-10-07 22:00:47 -0400

    code to install driver

--- tt_chrome_driver/tt_chrome_driver/chrome_driver.py
+++ tt_chrome_driver/tt_chrome_driver/chrome_driver.py
@@ -58,7 +58,8 @@ class ChromeDriver:
         zip_file = zipfile.ZipFile(urlretrieve(self.lookup['download_url'], file)[0], 'r')
         with zip_file as zf: zf.extractall(downloads)
         source = downloads.joinpath(list(filter(lambda s: 'LICENSE' not in s, zip_file.namelist()))[0])
-        replace(source, self.lookup['driver_folder'].joinpath('chromedriver' + self.lookup['driver_suffix']))
+        self.installed_driver_file = self.lookup['driver_folder'].joinpath('chromedriver' + self.lookup['driver_suffix'])
+        replace(source, self.installed_driver_file)
 
     def __init__(self):
         stable_version_url = 'https://googlechromelabs.github.io/chrome-for-testing/#stable'
