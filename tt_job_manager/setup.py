137f721a6caa1e4c6ee6daa89dda844978b4366e
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-10-15 21:20:57 -0400

    adding job manager

--- tt_job_manager/setup.py
+++ tt_job_manager/setup.py
@@ -4,5 +4,5 @@ setup(
     name='tt_job_manager',
     version='1.0',
     packages=find_packages(include=['tt_job_manager', 'tt_job_manager.*']),
-    install_requires=['tt_singleton', 'tt_semaphore', 'multiprocessing']
+    install_requires=['tt_singleton', 'tt_semaphore']
 )
