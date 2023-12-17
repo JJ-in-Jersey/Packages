e20a70c8925c362112a1d7c266fb5fd60f4ab258
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-12-16 23:28:12 -0500

    Refactoring to pull using requests rather than selenium

--- tt_noaa_data/tt_noaa_data/noaa_data.py
+++ tt_noaa_data/tt_noaa_data/noaa_data.py
@@ -28,12 +28,13 @@ def noaa_current_datafile(folder: Path, year: int, month: int, station, bin_num=
     return filepath
 
 
-def noaa_tide_datafile(folder: Path, year: int, month: int, station, bin_num=None):
+def noaa_tide_datafile(folder: Path, year: int, month: int, station):
 
     u1 = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date="
     u2 = "&end_date="
     u3 = "&station="
     u4 = "&product=high_low&datum=STND&time_zone=lst_ldt&units=english&format=xml"
+
 
     start_date = datetime(year, month, 1)
     if month < 12:
@@ -41,7 +42,7 @@ def noaa_tide_datafile(folder: Path, year: int, month: int, station, bin_num=Non
     else:
         end_date = datetime(year+1, 1, 1) - timedelta(days=1)
 
-    url = u1 + start_date.strftime("%Y%m%d") + u2 + end_date.strftime("%Y%m%d") + u3 + station + u4 + str(bin_num)
+    url = u1 + start_date.strftime("%Y%m%d") + u2 + end_date.strftime("%Y%m%d") + u3 + station
     response = requests.get(url)
     filepath = folder.joinpath(station + '_' + str(year) + '_' + str(month) + '.csv')
     with open(filepath, mode="wb") as file:
