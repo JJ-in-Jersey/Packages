4735c284f101d4a48fc6b856d1854366bc5d8632
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-01-28 13:29:26 -0500

    working on hell gate validations

--- tt_noaa_data/tt_noaa_data/noaa_data.py
+++ tt_noaa_data/tt_noaa_data/noaa_data.py
@@ -21,65 +21,115 @@ class TideXMLDataframe:
             self.frame.loc[len(self.frame)] = [date_time, date, time, hl]
 
 
-def noaa_current_fetch(start, end, folder: Path, station, bin_num=None):
+def noaa_current_fetch(start, end, folder: Path, station: str):
 
     if end.year != start.year:
         raise ValueError
 
+    station = station.split('_')[0]
+    bin_num = station.split('_')[1]
+    print(station, bin_num)
+
+    filepath = folder.joinpath(station + '_' + str(start.year) + '_current.csv')
     interval = 60
 
-    u1 = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date="
-    u2 = "&end_date="
-    u3 = "&station=" + station + "&product=currents_predictions&time_zone=lst_ldt&interval=" + str(interval) + "&units=english&format=csv"
-    u4 = "&bin=" + str(bin_num)
-
-    url_base = u1 + start.strftime("%Y%m%d") + u2 + end.strftime("%Y%m%d") + u3
-    url = url_base if not bin_num else url_base + u4
+    u1 = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=" + start.strftime("%Y%m%d")
+    u2 = "&end_date=" + end.strftime("%Y%m%d")
+    u3 = "&station=" + station
+    u4 = "&product=currents_predictions&time_zone=lst_ldt&interval=" + str(interval) + "&units=english&format=csv"
+    u5 = "&bin=" + str(bin_num)
+    url_base = u1 + u2 + u3 + u4
+    url = url_base if not bin_num else url_base + u5
 
     response = requests.get(url)
-    filepath = folder.joinpath(station + '_' + str(start.year) + '.csv')
     with open(filepath, mode="wb") as file:
         file.write(response.content)
 
     return filepath
 
 
-def noaa_current_dataframe(start, end, folder, station, bin_num=None):
+def noaa_current_dataframe(start, end, folder: Path, station: str):
+    # return speed of flowing water
 
     frame = pd.DataFrame()
 
     # start year
-    file = noaa_current_fetch(start, datetime(start.year, 12, 31), folder, station, bin_num)
+    file = noaa_current_fetch(start, datetime(start.year, 12, 31), folder, station)
     frame = pd.concat([frame, pd.read_csv(file, header='infer')])
 
     if start.year + 1 == end.year - 1:
-        file = noaa_current_fetch(datetime(start.year + 1, 1,1), datetime(start.year + 1, 12,31), folder, station, bin_num)
+        file = noaa_current_fetch(datetime(start.year + 1, 1,1), datetime(start.year + 1, 12,31), folder, station)
         frame = pd.concat([frame, pd.read_csv(file, header='infer')])
     else:
         for year in range(start.year + 1, end.year):
-            file = noaa_current_fetch(datetime(year, 1, 1), datetime(year, 12, 31), folder, station, bin_num)
+            file = noaa_current_fetch(datetime(year, 1, 1), datetime(year, 12, 31), folder, station)
             frame = pd.concat([frame, pd.read_csv(file, header='infer')])
 
     # end year
-    file = noaa_current_fetch(datetime(end.year, 1, 1), end, folder, station, bin_num)
+    file = noaa_current_fetch(datetime(end.year, 1, 1), end, folder, station)
     frame = pd.concat([frame, pd.read_csv(file, header='infer')])
 
     return frame
 
 
-def noaa_tide_dataframe(start, end, folder, station):
+def noaa_tide_dataframe(start, end, folder: Path, station: str):
+    # return height of flowing water
 
-    filepath = folder.joinpath(station + '.xml')
+    filepath = folder.joinpath(station + '_tide.xml')
 
-    u1 = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date="
-    u2 = "&end_date="
-    u3 = "&station=" + station + "&product=predictions&datum=STND&time_zone=lst_ldt&interval=hilo&units=english&format=xml"
+    u1 = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=" + start.strftime("%Y%m%d")
+    u2 = "&end_date=" + end.strftime("%Y%m%d")
+    u3 = "&station=" + station
+    u4 = "&product=predictions&datum=STND&time_zone=lst_ldt&interval=hilo&units=english&format=xml"
+    url = u1 + u2 + u3 + u4
 
-    url = u1 + start.strftime("%Y%m%d") + u2 + end.strftime("%Y%m%d") + u3
     response = requests.get(url)
     with open(filepath, mode="wb") as file:
         file.write(response.content)
 
     frame = TideXMLDataframe(filepath).frame
+
+    return frame
+
+def noaa_slack_fetch(start, end, folder: Path, station: str):
+
+    station = station.split('_')[0]
+    bin_num = station.split('_')[1]
+    print(station, bin_num)
+
+    filepath = folder.joinpath(station + '_slack.csv')
+
+    u1 = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=" + start.strftime("%Y%m%d")
+    u2 = "&end_date=" + end.strftime("%Y%m%d")
+    u3 = "&station=" + station
+    u4 = "&product=currents_predictions&time_zone=lst_ldt&interval=MAX_SLACK&units=english&format=csv"
+    url = u1 + u2 + u3 + u4
+
+    response = requests.get(url)
+    with open(filepath, mode="wb") as file:
+        file.write(response.content)
+
+    return filepath
+
+def noaa_slack_dataframe(start, end, folder: Path, station: str):
+    # return times of change in current, between high and low tide and close to zero
+
+    frame = pd.DataFrame()
+
+    # start year
+    file = noaa_slack_fetch(start, datetime(start.year, 12, 31), folder, station)
+    frame = pd.concat([frame, pd.read_csv(file, header='infer')])
+
+    if start.year + 1 == end.year - 1:
+        file = noaa_slack_fetch(datetime(start.year + 1, 1,1), datetime(start.year + 1, 12,31), folder, station)
+        frame = pd.concat([frame, pd.read_csv(file, header='infer')])
+    else:
+        for year in range(start.year + 1, end.year):
+            file = noaa_slack_fetch(datetime(year, 1, 1), datetime(year, 12, 31), folder, station)
+            frame = pd.concat([frame, pd.read_csv(file, header='infer')])
+
+    # end year
+    file = noaa_current_fetch(datetime(end.year, 1, 1), end, folder, station)
+    frame = pd.concat([frame, pd.read_csv(file, header='infer')])
 
     return frame
