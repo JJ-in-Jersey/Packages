1c8d1fc76bc1b66f1b49c0ca787335c72e8a7137
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-06-05 14:20:01 -0400

    fixing time_index

--- tt_date_time_tools/tt_date_time_tools/date_time_tools.py
+++ tt_date_time_tools/tt_date_time_tools/date_time_tools.py
@@ -8,10 +8,10 @@ def hours_mins(secs): return "%d:%02d" % (secs // 3600, secs % 3600 // 60)
 def mins_secs(secs): return "%d:%02d" % (secs // 60, secs % 60)
 
 
-def date_index(date): return int(pd.Timestamp(date).timestamp())
+def date_to_index(date): return int(pd.Timestamp(date).timestamp())
 
 
-def datetime(date_index): return pd.to_datetime(date_index, unit='s').round('min')
+def index_to_date(date_index): return pd.to_datetime(date_index, unit='s').round('min')
 
 
 def round_dt_quarter_hour(timestamp): return dt.min + round((timestamp.to_pydatetime() - dt.min) / td(minutes=15)) * td(minutes=15)
