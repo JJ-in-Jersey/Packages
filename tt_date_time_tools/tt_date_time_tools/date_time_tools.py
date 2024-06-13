feaf60e7369266fc7340bdf4d4816b31815d5ce6
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-06-12 22:30:02 -0400

    changed round_time to round_datetime

--- tt_date_time_tools/tt_date_time_tools/date_time_tools.py
+++ tt_date_time_tools/tt_date_time_tools/date_time_tools.py
@@ -14,7 +14,7 @@ def date_to_index(date): return int(pd.Timestamp(date).timestamp())
 def index_to_date(date_index): return pd.to_datetime(date_index, unit='s').round('min')
 
 
-def round_time(timestamp, mins=15): return dt.min + round((timestamp.to_pydatetime() - dt.min) / td(minutes=mins)) * td(minutes=mins)
+def round_datetime(timestamp, mins=15): return dt.min + round((timestamp.to_pydatetime() - dt.min) / td(minutes=mins)) * td(minutes=mins)
 
 
 def time_to_degrees(time): return time.hour * 15 + time.minute * 0.25
