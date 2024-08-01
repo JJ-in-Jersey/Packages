2237cab99d316e606ca64e043f8974241957d9de
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-08-01 10:08:15 -0400

    checking for string input to time_to_degree

--- tt_date_time_tools/tt_date_time_tools/date_time_tools.py
+++ tt_date_time_tools/tt_date_time_tools/date_time_tools.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import datetime as dt, timedelta as td
+import dateparser
 
 
 def hours_mins(secs): return "%d:%02d" % (secs // 3600, secs % 3600 // 60)
@@ -17,4 +18,7 @@ def index_to_date(date_index): return pd.to_datetime(date_index, unit='s').round
 def round_datetime(timestamp, mins=15): return dt.min + round((timestamp.to_pydatetime() - dt.min) / td(minutes=mins)) * td(minutes=mins)
 
 
-def time_to_degrees(time): return time.hour * 15 + time.minute * 0.25
+def time_to_degrees(time):
+    if isinstance(time, str):
+        time = dateparser.parse(time)
+    return time.hour * 15 + time.minute * 0.25
