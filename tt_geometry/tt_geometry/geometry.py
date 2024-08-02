02d3e978f35f60b503b660a23fbc403c81a74404
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-08-02 14:44:53 -0400

    new approach to creating next day arc

--- tt_geometry/tt_geometry/geometry.py
+++ tt_geometry/tt_geometry/geometry.py
@@ -7,11 +7,17 @@ class BaseArc:
 
     columns = None
     types = {}
+    midnight = pd.to_datetime(str(dt.today().date())).time()  # set to 00:00:00
 
     def info(self):
         return self.arc_dict
 
     def __init__(self, args):
+
+        self.fractured = False
+        self.zero_angle = False
+        self.next_day_arc = False
+
         self.arc_dict = {
             'start_date': None, 'min_date': None, 'end_date': None,
             'start_time': None, 'start_round_time': None, 'start_et': None,
@@ -39,38 +45,39 @@ class Arc(BaseArc):
     def __init__(self, args: dict):
         super().__init__(args)
 
-        self.fractured = False
-        self.start_day_arc = False
-        self.end_day_arc = False
-
         if self.arc_dict['start_date'] != self.arc_dict['end_date']:
             self.fractured = True
 
-            if not self.arc_dict['min_date'] == self.arc_dict['start_date']:
-                self.arc_dict['min_angle'] = None
-                self.arc_dict['min_time'] = None
-                self.arc_dict['min_round_time'] = None
-                self.arc_dict['min_round_angle'] = None
-                self.arc_dict['min_et'] = None
+        if self.arc_dict['start_angle'] == 0.0 or self.arc_dict['start_angle'] == 360.0:
+            self.zero_angle = True
 
-            if not self.arc_dict['start_angle'] == 0.0 and not self.arc_dict['start_angle'] == 360.0:
-                self.start_day_arc = True
-                self.arc_dict['end_angle'] = 360
-                self.arc_dict['end_round_angle'] = self.arc_dict['end_angle']
-                self.arc_dict['end_time'] = pd.to_datetime(str(dt.today().date())).time()  # set to 00:00:00
-                self.arc_dict['end_round_time'] = self.arc_dict['end_time']
-                self.arc_dict['end_et'] = None
+        if not self.arc_dict['end_angle'] == 360.0 and not self.arc_dict['end_angle'] == 0.0:
+            self.next_day_arc = True
 
-                if self.arc_dict['min_angle'] == 0:
-                    self.arc_dict['min_angle'] = 360
-                    self.arc_dict['min_round_angle'] = self.arc_dict['min_angle']
+        # end of the arc is in the next day and should be set to 00:00:00 for this day
+        if self.fractured and not self.zero_angle:
+            self.arc_dict['end_angle'] = 360
+            self.arc_dict['end_round_angle'] = 360
+            self.arc_dict['end_time'] = BaseArc.midnight
+            self.arc_dict['end_round_time'] = BaseArc.midnight
+            self.arc_dict['end_et'] = None
 
-                # self.start_day_arc = FractionalArcStartDay(self)
-            if not self.arc_dict['end_angle'] == 360.0 and not self.arc_dict['end_angle'] == 0.0:
-                self.end_day_arc = True
-                self.arc_dict['start_date'] = self.arc_dict['start_date'] + td(days=1)
-                self.arc_dict['start_time'] = pd.to_datetime(str(dt.today().date())).time()  # set to 00:00:00
-                self.arc_dict['start_angle'] = 0
-                self.arc_dict['start_round_time'] = self.arc_dict['start_time']
-                self.arc_dict['start_round_angle'] = self.arc_dict['start_angle']
-                self.arc_dict['start_et'] = None
+        #  minimum is in the next day, so this day minimum should be zeroed out
+        if (self.fractured and not self.zero_angle and
+                not self.arc_dict['min_date'] == self.arc_dict['start_date']):
+            self.arc_dict['min_angle'] = None
+            self.arc_dict['min_time'] = None
+            self.arc_dict['min_round_time'] = None
+            self.arc_dict['min_round_angle'] = None
+            self.arc_dict['min_et'] = None
+
+        #  create new arc for the next day
+        if self.fractured and not self.zero_angle and self.next_day_arc:
+            new_args = dict(args)
+            new_args['start_date'] = new_args['start_date'] + td(days=1)
+            new_args['start_time'] = BaseArc.midnight
+            new_args['start_round_time'] = BaseArc.midnight
+            new_args['start_angle'] = 0
+            new_args['start_round_angle'] = 0
+            new_args['start_et'] = None
+            self.next_day_arc = Arc(new_args)
