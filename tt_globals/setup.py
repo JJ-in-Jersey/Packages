3d5b7dcf4f8320b1d8a68d13caa009a1c3b6f0d4
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-08-02 16:14:00 -0400

    adding dateparser dependency

--- tt_globals/setup.py
+++ tt_globals/setup.py
@@ -4,5 +4,5 @@ setup(
     name='tt_globals',
     version='1.1',
     packages=find_packages(include=['tt_globals', 'tt_globals.*']),
-    install_requires=['num2words']
+    install_requires=['num2words', 'dateparser']
 )
