b1a6826ee66f2952c8ef5ea917aa4acd14020381
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-08-17 08:17:30 -0400

    adding logical flags for start and end and min

--- tt_geometry/tt_geometry/geometry.py
+++ tt_geometry/tt_geometry/geometry.py
@@ -41,12 +41,13 @@ class Arc(BaseArc):
     def __init__(self, args: dict):
         super().__init__(args)
 
-        end_day_one = self.arc_dict['start_datetime'].date() == self.arc_dict['end_datetime'].date()
-        end_equals_midnight = self.arc_dict['end_round_angle'] == 360.0 or self.arc_dict['end_round_angle'] == 0.0
+        start_and_end_on_same_day = self.arc_dict['start_datetime'].date() == self.arc_dict['end_datetime'].date()
+        ends_at_midnight = self.arc_dict['end_round_angle'] == 360.0 or self.arc_dict['end_round_angle'] == 0.0
+        start_and_min_on_same_day = self.arc_dict['start_datetime'].date() == self.arc_dict['min_datetime'].date()
 
         # absolutely must split into to arcs, implies creation of a next_day_arc
         # start day and end day are different and arc doesn't end just over the line on the next day
-        if not end_day_one and not end_equals_midnight:
+        if not start_and_end_on_same_day and not ends_at_midnight:
 
             #  copy args for next day arc
             new_args = deepcopy(args)
@@ -67,13 +68,13 @@ class Arc(BaseArc):
 
             if not self.arc_dict['min_datetime'] is None:
                 #  if minima is in the start day, zero out the minima for the next day
-                if self.arc_dict['min_datetime'].date() == self.arc_dict['start_datetime'].date():
+                if start_and_min_on_same_day:
                     new_args['min_angle'] = None
                     new_args['min_datetime'] = None
                     new_args['min_round_datetime'] = None
                     new_args['min_round_angle'] = None
                     new_args['min_et'] = None
-                elif self.arc_dict['min_datetime'].date() == self.arc_dict['end_datetime'].date():
+                elif not start_and_min_on_same_day:
                     #  if minima is in the next day, zero out start day minima
                     self.arc_dict['min_angle'] = None
                     self.arc_dict['min_datetime'] = None
@@ -84,7 +85,7 @@ class Arc(BaseArc):
             self.next_day_arc = Arc(new_args)
 
         #  arc starts on first day and ends just over the line the next day, reset end date to first day
-        if not end_day_one and end_equals_midnight:
+        if not start_and_end_on_same_day and ends_at_midnight:
             self.arc_dict['end_datetime'] = self.arc_dict['start_datetime'].replace(hour=0, minute=0)  # force end date to start date
             self.arc_dict['end_round_datetime'] = self.arc_dict['end_datetime']
             self.arc_dict['end_angle'] = 360
@@ -93,7 +94,7 @@ class Arc(BaseArc):
         # zero angle arc - start and end are on the same day and the angle difference is 0 or 360
         # doesn't matter if their midnight or not
         # purge from list later
-        if end_day_one:
+        if start_and_end_on_same_day:
             angle_round_difference = self.arc_dict['end_round_angle'] - self.arc_dict['start_round_angle']
             if angle_round_difference == 360.0 or angle_round_difference == 0:
                 self.zero_angle = True
