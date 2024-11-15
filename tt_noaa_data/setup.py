cceb6aeedbc433e3164d593b41f8a5b7e67902e3
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-11-14 22:59:09 -0500

    fixed '' problem in requires=

--- tt_noaa_data/setup.py
+++ tt_noaa_data/setup.py
@@ -4,5 +4,5 @@ setup(
     name='tt_noaa_data',
     version='2.0',
     packages=find_packages(include=['tt_noaa_data', 'tt_noaa_data.*']),
-    install_requires=['requests, tt_globals']
+    install_requires=['requests', 'tt_globals']
 )
