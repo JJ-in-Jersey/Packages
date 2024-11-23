69896c8f70754a33b8d8e054a543d5fc6295ce37
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-11-23 13:09:57 -0500

    Changed package build order

--- bin/install_packages.bat
+++ bin/install_packages.bat
@@ -15,6 +15,7 @@ pip uninstall tt_globals -y
 pip uninstall tt_jobs -y
 pip uninstall tt_gpx -y
 
+pip install ./tt_globals
 pip install ./tt_singleton
 pip install ./tt_navigation
 pip install ./tt_date_time_tools
@@ -26,7 +27,6 @@ pip install ./tt_semaphore
 pip install ./tt_chrome_driver
 pip install ./tt_job_manager
 pip install ./tt_noaa_data
-pip install ./tt_globals
 pip install ./tt_jobs
 pip install ./tt_gpx
 
