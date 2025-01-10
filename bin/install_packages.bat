26c37dfe270ded55e04e7ad0fedd578eac985214
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-01-10 11:33:52 -0500

    sorted installations alphabetically

--- bin/install_packages.bat
+++ bin/install_packages.bat
@@ -16,9 +16,9 @@ pip uninstall tt_singleton -y
 pip uninstall tt_chrome_driver -y
 
 pip install ./tt_singleton
+pip install ./tt_os_abstraction
 pip install ./tt_semaphore
 pip install ./tt_navigation
-pip install ./tt_os_abstraction
 
 pip install ./tt_date_time_tools
 pip install ./tt_file_tools
