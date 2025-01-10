e33c072b7cb49dc48c83274127670d7a2949c3ec
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-01-10 11:24:37 -0500

    sorted installations alphabetically

--- bin/install_packages.bat
+++ bin/install_packages.bat
@@ -17,7 +17,6 @@ pip uninstall tt_chrome_driver -y
 
 pip install ./tt_singleton
 pip install ./tt_navigation
-pip install ./tt_jobs
 pip install ./tt_date_time_tools
 pip install ./tt_file_tools
 pip install ./tt_geometry
@@ -29,5 +28,6 @@ pip install ./tt_os_abstraction
 pip install ./tt_noaa_data
 pip install ./tt_semaphore
 REM pip install ./tt_chrome_driver
+REM pip install ./tt_jobs
 
 pause
