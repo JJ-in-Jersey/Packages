87e99ff599aa7033a14f5cf82a327bb812857901
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-01-10 11:13:21 -0500

    sorted installations alphabetically

--- bin/install_packages.bat
+++ bin/install_packages.bat
@@ -1,33 +1,33 @@
 cd %userprofile%/PycharmProjects/Packages
 
-pip uninstall tt_singleton -y
-pip uninstall tt_navigation -y
 pip uninstall tt_date_time_tools -y
 pip uninstall tt_file_tools -y
 pip uninstall tt_geometry -y
-pip uninstall tt_interpolation -y
-pip uninstall tt_os_abstraction -y
-pip uninstall tt_semaphore -y
-pip uninstall tt_chrome_driver -y
-pip uninstall tt_job_manager -y
-pip uninstall tt_noaa_data -y
 pip uninstall tt_globals -y
-pip uninstall tt_jobs -y
 pip uninstall tt_gpx -y
+pip uninstall tt_interpolation -y
+pip uninstall tt_job_manager -y
+pip uninstall tt_jobs -y
+pip uninstall tt_os_abstraction -y
+pip uninstall tt_navigation -y
+pip uninstall tt_noaa_data -y
+pip uninstall tt_semaphore -y
+pip uninstall tt_singleton -y
+pip uninstall tt_chrome_driver -y
 
-pip install ./tt_globals
-pip install ./tt_singleton
-pip install ./tt_navigation
 pip install ./tt_date_time_tools
 pip install ./tt_file_tools
 pip install ./tt_geometry
-pip install ./tt_interpolation
-pip install ./tt_os_abstraction
-pip install ./tt_semaphore
-pip install ./tt_chrome_driver
-pip install ./tt_job_manager
-pip install ./tt_noaa_data
-pip install ./tt_jobs
+pip install ./tt_globals
 pip install ./tt_gpx
+pip install ./tt_interpolation
+pip install ./tt_job_manager
+pip install ./tt_jobs
+pip install ./tt_os_abstraction
+pip install ./tt_navigation
+pip install ./tt_noaa_data
+pip install ./tt_semaphore
+pip install ./tt_singleton
+REM pip install ./tt_chrome_driver
 
 pause
