c75a00726d66f016d297b921283db05a28aa2e50
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-01-10 21:26:22 -0500

    removing unnecessary comments

--- tt_job_manager/tt_job_manager/__init__.py
+++
@@ -1,6 +0,0 @@
-# from multiprocessing import set_start_method
-# import tt_job_manager.job_manager as jm
-#
-# if __name__ == '__main__':
-#     set_start_method('spawn')
-#     job_manager = jm.JobManager()
