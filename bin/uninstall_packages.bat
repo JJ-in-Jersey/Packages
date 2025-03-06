4f207187a4aa6192980c719bae9289e05bc31e11
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-03-05 19:41:21 -0500

    new file

---
+++ bin/uninstall_packages.bat
@@ -0,0 +1,20 @@
+pip cache purge
+
+cd %userprofile%/PycharmProjects/Packages
+
+pip uninstall tt_date_time_tools -y
+pip uninstall tt_file_tools -y
+pip uninstall tt_geometry -y
+pip uninstall tt_globals -y
+pip uninstall tt_gpx -y
+pip uninstall tt_interpolation -y
+pip uninstall tt_job_manager -y
+pip uninstall tt_jobs -y
+pip uninstall tt_os_abstraction -y
+pip uninstall tt_navigation -y
+pip uninstall tt_noaa_data -y
+pip uninstall tt_semaphore -y
+pip uninstall tt_singleton -y
+pip uninstall tt_chrome_driver -y
+
+pause
