0b0162f9677314231855c2a370e3637815433d10
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-04 13:21:09 -0400

    expanded and added explicit cd

--- tt_dataframe/reinstall.bat
+++ tt_dataframe/reinstall.bat
@@ -3,8 +3,8 @@ python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pip cache purge
 cd %userprofile%/PycharmProjects/Packages
 
-pip uninstall tt_file_tools -y
-pip install ./tt_file_tools
+pip uninstall tt_dataframe -y
+pip install ./tt_dataframe
 
 python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
