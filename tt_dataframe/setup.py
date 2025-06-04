75677be58018f84d7c86e5821c8876c04f52229a
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-04 13:21:31 -0400

    add exceptions dependency

---
+++ tt_dataframe/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tt_dataframe',
+    packages=find_packages(include=['tt_dataframe', 'tt_dataframe.*']),
+    install_requires=['tt_exceptions']
+)
