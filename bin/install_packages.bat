6549627113fca6051d05c1d26dccf18ecf99055b
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-04 10:58:36 -0400

    removed uninstall so that install only installs packages

--- bin/install_packages.bat
+++ bin/install_packages.bat
@@ -4,22 +4,6 @@ python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pip cache purge
 cd %userprofile%/PycharmProjects/Packages
 
-pip uninstall tt_date_time_tools -y
-pip uninstall tt_file_tools -y
-pip uninstall tt_exceptions -y
-pip uninstall tt_geometry -y
-pip uninstall tt_globals -y
-pip uninstall tt_gpx -y
-pip uninstall tt_interpolation -y
-pip uninstall tt_job_manager -y
-pip uninstall tt_jobs -y
-pip uninstall tt_os_abstraction -y
-pip uninstall tt_navigation -y
-pip uninstall tt_noaa_data -y
-pip uninstall tt_semaphore -y
-pip uninstall tt_singleton -y
-pip uninstall tt_chrome_driver -y
-
 pip install ./tt_interpolation
 pip install ./tt_os_abstraction
 pip install ./tt_navigation
