f707594e2fa01fad516990249e39dd4bf3c0d149
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-04 10:59:17 -0400

    add explicit cd so that execution works when running as admin

--- tt_exceptions/reinstall.bat
+++ tt_exceptions/reinstall.bat
@@ -1,5 +1,10 @@
-cd ..
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
+
+pip cache purge
+cd %userprofile%/PycharmProjects/Packages
+
 pip uninstall tt_exceptions -y
 pip install ./tt_exceptions
 
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
