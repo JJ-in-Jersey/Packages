9ab6bb6d6864aa627948a74cb99a8f3faa083f0e
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-04 11:00:38 -0400

    added FileIsEmpty and FileSampleIsEmpty

--- tt_exceptions/tt_exceptions/exceptions.py
+++ tt_exceptions/tt_exceptions/exceptions.py
@@ -32,3 +32,15 @@ class SplineCSVFailed(Exception):
     def __init__(self, message: str):
         self.message = message
         super().__init__(message)
+
+
+class FileIsEmpty(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
+
+
+class FileSampleIsEmpty(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
