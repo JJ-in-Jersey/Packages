3a604f12adb34a5005138a768fbe33418f6dc6a8
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-05 12:43:56 -0400

    added explicit cd and cleanup

--- tt_noaa_data/reinstall.bat
+++ tt_noaa_data/reinstall.bat
@@ -1,5 +1,10 @@
-cd ..
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
+
+pip cache purge
+cd %userprofile%/PycharmProjects/Packages
+
 pip uninstall tt_noaa_data -y
 pip install ./tt_noaa_data
 
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
