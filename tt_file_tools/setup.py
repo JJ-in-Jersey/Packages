f87366189902267e380b228e4d3d0f94a7d1a385
JJ <118015245+JJ-in-Jersey@users.noreply.github.com>
2025-06-05 12:51:51 -0400

    Delete tt_file_tools directory

--- tt_file_tools/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='tt_file_tools',
-    packages=find_packages(include=['tt_file_tools', 'tt_file_tools.*']),
-    install_requires=['numpy', 'pandas', 'bs4']
-)
