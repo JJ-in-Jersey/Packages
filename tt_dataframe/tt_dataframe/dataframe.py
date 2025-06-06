491fccf6391c45d06ad2015ad06d3dddddf0aadf
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-05 22:49:01 -0400

    finally got isna to work by ignoring claude's advice

--- tt_dataframe/tt_dataframe/dataframe.py
+++ tt_dataframe/tt_dataframe/dataframe.py
@@ -1,51 +1,34 @@
-from pandas import DataFrame as PandasDataFrame
+from pandas import DataFrame as PandasDataFrame, read_csv
 from pathlib import Path
-from io import StringIO, BytesIO
-from typing import Union, TextIO, BinaryIO
+from io import StringIO
 
 class DataFrame(PandasDataFrame):
 
-    _spreadsheet_row_limit = 950000
+    def __init__(self, data=None, index=None, columns=None, dtype=None, copy=None, csv_source: Path | StringIO = None):
 
-    @property
-    def row_limit(self):
-        return DataFrame._spreadsheet_row_limit
+        if csv_source is not None:
+            data = read_csv(csv_source)
+        super().__init__(data=data, index=index, columns=columns, dtype=dtype, copy=copy)
 
     @property
     def _constructor(self):
         return DataFrame
 
-    def __init__(self, source: Union[str, Path, TextIO, BinaryIO, StringIO, BytesIO] = None,
-                 data=None, *args, **kwargs):
+    def write(self, csv_target: Path, **kwargs):
+        self.to_csv(csv_target, **kwargs)
+        return csv_target
 
-        # Separate pandas DataFrame kwargs from read_csv kwargs
-        df_specific_keys = {'index', 'columns', 'dtype', 'copy'}
-        df_kwargs = {k: v for k, v in kwargs.items() if k in df_specific_keys}
-        read_csv_kwargs = {k: v for k, v in kwargs.items() if k not in df_specific_keys}
-
-        if source is not None:
-            df_data = PandasDataFrame.read_csv(source, *args, **read_csv_kwargs)  # read_csv handles files & IO identically
-            super().__init__(df_data, **df_kwargs)
-        elif data is not None:
-            super().__init__(data, *args, **kwargs)
-        else:
-            super().__init__(*args, **kwargs)
-
-    def save_to_csv(self, file_path: Union[str, Path], **kwargs):
-        self.to_csv(file_path, **kwargs)
-        return file_path
-
-    def save_to_csvs(self, file_path: Union[str, Path], **kwargs):
-        suffix = '.csv'
-        if len(self) > self.row_limit:
-            num_of_spreadsheets = len(self) / self.row_limit
-            whole_spreadsheets = len(self) // self.row_limit
-            for i in range(whole_spreadsheets):
-                output_stem = file_path.parent.joinpath(file_path.stem + '_spreadsheet_' + str(i))
-                temp = self.loc[i * self.row_limit: i * self.row_limit + self.row_limit - 1]
-                temp.to_csv(output_stem.with_suffix(output_stem.suffix + suffix), index=False)
-            if num_of_spreadsheets > whole_spreadsheets:
-                output_stem = file_path.parent.joinpath(file_path.stem + '_spreadsheet_' + str(whole_spreadsheets))
-                temp = self.loc[whole_spreadsheets * self.row_limit:]
-                temp.to_csv(output_stem.with_suffix(output_stem.suffix + suffix), index=False)
-        return file_path
+    # def save_to_csvs(self, file_path: Union[str, Path], **kwargs):
+    #     suffix = '.csv'
+    #     if len(self) > self.row_limit:
+    #         num_of_spreadsheets = len(self) / self.row_limit
+    #         whole_spreadsheets = len(self) // self.row_limit
+    #         for i in range(whole_spreadsheets):
+    #             output_stem = file_path.parent.joinpath(file_path.stem + '_spreadsheet_' + str(i))
+    #             temp = self.loc[i * self.row_limit: i * self.row_limit + self.row_limit - 1]
+    #             temp.to_csv(output_stem.with_suffix(output_stem.suffix + suffix), index=False)
+    #         if num_of_spreadsheets > whole_spreadsheets:
+    #             output_stem = file_path.parent.joinpath(file_path.stem + '_spreadsheet_' + str(whole_spreadsheets))
+    #             temp = self.loc[whole_spreadsheets * self.row_limit:]
+    #             temp.to_csv(output_stem.with_suffix(output_stem.suffix + suffix), index=False)
+    #     return file_path
