1fd74e5d6d148b46e1b56fc8b348c0c67da2b13f
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-06 22:30:27 -0400

    explicit cd

--- tt_interpolation/reinstall.bat
+++ tt_interpolation/reinstall.bat
@@ -1,5 +1,10 @@
-cd ..
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
+
+pip cache purge
+cd %userprofile%/PycharmProjects/Packages
+
 pip uninstall tt_interpolation -y
 pip install ./tt_interpolation
 
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
