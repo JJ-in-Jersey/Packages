1fdb0442805b814b16a9bfa9f4899db298cfe728
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-10 21:40:10 -0400

    added explicit cd

--- tt_gpx/reinstall.bat
+++ tt_gpx/reinstall.bat
@@ -1,5 +1,10 @@
-cd ..
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
+
+pip cache purge
+cd %userprofile%/PycharmProjects/Packages
+
 pip uninstall tt_gpx -y
 pip install ./tt_gpx
 
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
