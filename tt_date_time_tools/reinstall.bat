761f4573e5c4e660708ddce5c84f7ac4a7080edb
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-12 09:55:07 -0400

    explicit cd with purge

--- tt_date_time_tools/reinstall.bat
+++ tt_date_time_tools/reinstall.bat
@@ -1,5 +1,10 @@
-cd ..
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
+
+pip cache purge
+cd %userprofile%/PycharmProjects/Packages
+
 pip uninstall tt_date_time_tools -y
 pip install ./tt_date_time_tools
 
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
--- tt_globals/reinstall.bat
+++ tt_globals/reinstall.bat
@@ -1,8 +1,10 @@
-for /f "tokens=1" %%a in ('cd') do (set pwd=%%a)
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 
-cd ..
+pip cache purge
+cd %userprofile%/PycharmProjects/Packages
+
 pip uninstall tt_globals -y
 pip install ./tt_globals
 
-cd %pwd%
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
