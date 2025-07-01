8df60bc0098b60c15d5c41dfe6905d7a358982ee
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-07-01 17:51:43 -0400

    removed DuplicateValues and LengthMismatch

--- tt_geometry/tt_geometry/geometry.py
+++ tt_geometry/tt_geometry/geometry.py
@@ -2,8 +2,6 @@ from datetime import timedelta as td
 from tt_date_time_tools.date_time_tools import time_to_degrees
 from copy import deepcopy
 
-from tt_exceptions.exceptions import DateMismatch
-
 
 class Arc:
 
@@ -27,24 +25,18 @@ class Arc:
         self.end_duration_display = True
 
         self.start_angle = time_to_degrees(self.start_time)
-        if abs(self.start_angle) % 360 <= self.midnight_tolerance:
+        if self.start_angle <= self.midnight_tolerance:
             self.start_duration_display = False
 
         self.min_angle = time_to_degrees(self.min_time)
-        if abs(self.min_angle) % 360 <= self.midnight_tolerance:
+        if self.min_angle <= self.midnight_tolerance:
             self.min_duration_display = False
 
         self.end_angle = time_to_degrees(self.end_time)
-        if abs(self.end_angle) % 360 <= self.midnight_tolerance:
+        if self.end_angle <= self.midnight_tolerance:
             self.end_duration_display = False
 
-        self.total_angle = abs(self.end_angle - self.start_angle) % 360
-
-        if self.start_time.date() != self.end_time.date():
-            raise DateMismatch(f'     {self.__class__} start and end days are different')
-
-        # if self.total_angle % 360 <= 0:
-        #     raise LengthMismatch(f'     {self.__class__} Ignoring arc length pf {self.total_angle} degrees, {self.total_angle/0.25} minutes')
+        self.total_angle = min(abs(self.end_angle - self.start_angle), 360 - abs(self.end_angle - self.start_angle))
 
 
 class StartArc(Arc):
