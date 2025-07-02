c2b17e0ef752f22095b5bb954814b641c29d0b20
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-07-01 20:53:27 -0400

    added explicit cd

--- tt_jobs/reinstall.bat
+++ tt_jobs/reinstall.bat
@@ -1,5 +1,10 @@
-cd ..
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
+
+pip cache purge
+cd %userprofile%/PycharmProjects/Packages
+
 pip uninstall tt_jobs -y
 pip install ./tt_jobs
 
+python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
