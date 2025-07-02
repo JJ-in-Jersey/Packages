9c9fbce7d5b20ca866a89e81ade7dfc9f699e4fe
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-07-01 22:16:27 -0400

    removed dependencies

--- tt_jobs/setup.py
+++ tt_jobs/setup.py
@@ -2,6 +2,5 @@ from setuptools import setup, find_packages
 
 setup(
     name='tt_jobs',
-    packages=find_packages(include=['tt_jobs', 'tt_jobs.*']),
-    install_requires=['sympy', 'tt_interpolation', 'tt_job_manager']
+    packages=find_packages(include=['tt_jobs', 'tt_jobs.*'])
 )
