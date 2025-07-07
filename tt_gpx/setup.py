b4567b1dc8a66c8f7a71b84d49099ad690b8872d
jason <jason.bronfeld@pm.me>
2025-07-07 09:47:08 -0500

    typo

--- tt_gpx/setup.py
+++ tt_gpx/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='tt_gpx',
     packages=find_packages(include=['tt_gpx', 'tt_gpx.*']),
-    install_requires=['Beautifulsoup4', 'lxml', 'num2words', 'tt_globals', 'tt_navigation', 'tt_file_tools']
+    install_requires=['beautifulsoup4', 'lxml', 'num2words', 'tt_globals', 'tt_navigation', 'tt_file_tools']
 )
