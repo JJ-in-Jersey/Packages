5c2cd577ddd5a9464f326b03b330b9b1ada10a47
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-07-09 15:16:32 -0400

    added None as argument type to silence PyCharm type checker

--- tt_date_time_tools/tt_date_time_tools/date_time_tools.py
+++ tt_date_time_tools/tt_date_time_tools/date_time_tools.py
@@ -8,7 +8,7 @@ def hours_mins(secs): return "%d:%02d" % (secs // 3600, secs % 3600 // 60)
 def mins_secs(secs): return "%d:%02d" % (secs // 60, secs % 60)
 
 
-def time_to_degrees(time: str | Timestamp):
+def time_to_degrees(time: str | Timestamp | None):
     if isinstance(time, str):
         time = dateparser.parse(time)
         return (time.hour * 15 + time.minute * 0.25) % 360
