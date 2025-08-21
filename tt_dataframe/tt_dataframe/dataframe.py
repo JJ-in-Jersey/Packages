bc123010cdbf99af5be8c18efd70fae6bd7b1835
jj-in-jersey <faircurrents@gmail.com>
2025-08-21 18:50:54 -0400

    removed commented out code

--- tt_dataframe/tt_dataframe/dataframe.py
+++ tt_dataframe/tt_dataframe/dataframe.py
@@ -19,18 +19,3 @@ class DataFrame(PandasDataFrame):
             kwargs['index'] = False
         self.to_csv(csv_target, **kwargs)
         return csv_target
-
-    # def save_to_csvs(self, file_path: Union[str, Path], **kwargs):
-    #     suffix = '.csv'
-    #     if len(self) > self.row_limit:
-    #         num_of_spreadsheets = len(self) / self.row_limit
-    #         whole_spreadsheets = len(self) // self.row_limit
-    #         for i in range(whole_spreadsheets):
-    #             output_stem = file_path.parent.joinpath(file_path.stem + '_spreadsheet_' + str(i))
-    #             temp = self.loc[i * self.row_limit: i * self.row_limit + self.row_limit - 1]
-    #             temp.to_csv(output_stem.with_suffix(output_stem.suffix + suffix), index=False)
-    #         if num_of_spreadsheets > whole_spreadsheets:
-    #             output_stem = file_path.parent.joinpath(file_path.stem + '_spreadsheet_' + str(whole_spreadsheets))
-    #             temp = self.loc[whole_spreadsheets * self.row_limit:]
-    #             temp.to_csv(output_stem.with_suffix(output_stem.suffix + suffix), index=False)
-    #     return file_path
