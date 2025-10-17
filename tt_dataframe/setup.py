4b43a05054021509b5171ccfa22329aad8e93b01
jj-in-jersey <faircurrents@gmail.com>
2025-10-17 00:13:31 -0400

    added polars to setup for dataframe to speed reconstruction of tuples in csv

--- tt_dataframe/setup.py
+++ tt_dataframe/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='tt_dataframe',
     packages=find_packages(include=['tt_dataframe', 'tt_dataframe.*']),
-    install_requires=['tt_exceptions']
+    install_requires=['tt_exceptions', 'polars']
 )
