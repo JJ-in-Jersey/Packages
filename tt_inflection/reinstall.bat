9d9492559a4b985f751ff925ab13f911614554e0
jj-in-jersey <faircurrents@gmail.com>
2025-10-17 23:13:36 -0400

    deleted inflection

--- tt_inflection/reinstall.bat
+++
@@ -1,8 +0,0 @@
-for /f "tokens=1" %%a in ('cd') do (set pwd=%%a)
-
-cd ..
-pip uninstall tt_inflection -y
-pip install ./tt_inflection
-
-cd %pwd%
-pause
--- tt_inflection/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='tt_inflection',
-    packages=find_packages(include=['tt_inflection', 'tt_inflection.*']),
-)
