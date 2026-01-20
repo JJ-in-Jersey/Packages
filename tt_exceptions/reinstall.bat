15b0cb451349147121599f573a1cc6c37828769f
albatross <hercules.class011@passinbox.com>
2026-01-20 18:41:57 -0500

    changed pip to python -m pip in reinstall.bat

--- tt_exceptions/reinstall.bat
+++ tt_exceptions/reinstall.bat
@@ -1,10 +1,10 @@
 python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 
-pip cache purge
+python -m pip cache purge
 cd %userprofile%/PycharmProjects/Packages
 
-pip uninstall tt_exceptions -y
-pip install ./tt_exceptions
+python -m pip uninstall tt_exceptions -y
+python -m pip install ./tt_exceptions
 
 python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
