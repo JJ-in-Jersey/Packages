742d8c275e164e838b32be0a520ed6d323018187
albatross <hercules.class011@passinbox.com>
2026-01-19 21:56:21 -0500

    fixed reinstall.bat python -m pip

--- tt_noaa_data/reinstall.bat
+++ tt_noaa_data/reinstall.bat
@@ -1,10 +1,10 @@
 python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 
-pip cache purge
+python -m pip cache purge
 cd %userprofile%/PycharmProjects/Packages
 
-pip uninstall tt_noaa_data -y
-pip install ./tt_noaa_data
+python -m pip uninstall tt_noaa_data -y
+python -m pip install ./tt_noaa_data
 
 python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
