ba857b98f747ec24754b79dfccd5b3c167d9bbcf
albatross <hercules.class011@passinbox.com>
2026-01-20 21:42:25 -0500

    added python -m pip to reinstall.bat

--- tt_jobs/reinstall.bat
+++ tt_jobs/reinstall.bat
@@ -1,10 +1,10 @@
 python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 
-pip cache purge
+python -m pip cache purge
 cd %userprofile%/PycharmProjects/Packages
 
-pip uninstall tt_jobs -y
-pip install ./tt_jobs
+python -m pip uninstall tt_jobs -y
+python -m pip install ./tt_jobs
 
 python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
