066e3f8e35766c5d2d065c3d16603b94509e00c9
albatross <hercules.class011@passinbox.com>
2026-01-22 07:32:58 -0500

    parallelized the downloading as well as the spline fitting in noaa_data.py

--- tt_noaa_data/tt_noaa_data/noaa_data.py
+++ tt_noaa_data/tt_noaa_data/noaa_data.py
@@ -13,58 +13,41 @@ import tt_globals.globals as fc_globals
 from tt_gpx.gpx import Waypoint
 from tt_exceptions.exceptions import EmptyResponse, DuplicateValues, NonMonotonic, PredictionsNotAvailable
 
+from tt_job_manager.job_manager import JobManager
+
 class StationDict(Dictionary):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    @classmethod
+    def _convert_to_this(cls, json_dict: dict):
+        return dict(json_dict)
 
-        if not fc_globals.STATIONS_FILE.exists():
-            my_request = "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.xml?type=currentpredictions&units=english"
-            for _ in range(5):
-                try:
-                    print(f'Requesting list of stations')
-                    my_response = requests.get(my_request)
-                    my_response.raise_for_status()
-                    stations_tree = SoupFromXMLResponse(StringIO(my_response.content.decode())).soup
-                    row_array = [{'id': station_tag.find_next('id').text,
-                                  'name': station_tag.find_next('name').text,
-                                  'lat': float(station_tag.find_next('lat').text),
-                                  'lon': float(station_tag.find_next('lng').text),
-                                  'type': station_tag.find_next('type').text,
-                                  'folder_name': station_tag.find_next('type').text + ' ' + station_tag.find_next('id').text}
-                                 for station_tag in stations_tree.find_all('Station')]
-                    row_df = DataFrame(row_array).drop_duplicates()
-                    row_df['folder'] = row_df['folder_name'].apply(self.absolute_path_string)
-                    row_dict = row_df.to_dict('records')
-                    self.update({r['id']: r for r in row_dict})
+    def __init__(self, job_manager: JobManager = None):
+        if fc_globals.STATIONS_FILE.exists():
+            super().__init__(json_source=fc_globals.STATIONS_FILE)
+            return
 
-                    print(f'Requesting bins for each station')
-                    for station_id in self.keys():
-                        print(f'==>   {station_id}')
-                        my_request = "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations/" + station_id + "/bins.xml?units=english"
-                        for _ in range(3):
-                            try:
-                                my_response = requests.get(my_request)
-                                my_response.raise_for_status()
-                                bins_tree = SoupFromXMLResponse(StringIO(my_response.content.decode())).soup
-                                bin_count = int(bins_tree.find("nbr_of_bins").text)
-                                if bin_count and bins_tree.find('Bin').find('depth') is not None:
-                                    bin_dict = {int(tag.num.text): float(tag.depth.text) for tag in bins_tree.find_all('Bin')}
-                                    bin_dict = dict(sorted(bin_dict.items(), key=lambda item: item[1]))
-                                    self[station_id]['bins'] = bin_dict
-                                break
-                            except requests.exceptions.RequestException:
-                                sleep(1)
-                    print_file_exists(self.write(fc_globals.STATIONS_FILE))
-                    break
-                except requests.exceptions.RequestException:
-                    sleep(1)
+        super().__init__()
+        if not self and job_manager is not None:
 
+            self.get_request()
+
+            from tt_jobs.jobs import RequestBinJob
+            keys = [job_manager.submit_job(RequestBinJob(station_id)) for station_id in self.keys()]
+            # for station_id in self.keys():
+            #     job = RequestBinJob(station_id)
+            #     result = job.execute()
+            job_manager.wait()
+
+            for station_id in keys:
+                self[station_id]['bins'] = job_manager.get_result(station_id)
+
+            self.write(fc_globals.STATIONS_FILE)
+        else:
+            raise SyntaxError("StationDict requires existing STATIONS_FILE or JobManager")
 
     @staticmethod
     def absolute_path_string(folder_name):
         return str(fc_globals.WAYPOINTS_FOLDER.joinpath(folder_name).absolute())
-
 
     def add_waypoint(self, route_waypoint: Waypoint):
         row = {'id': route_waypoint.id, 'name': route_waypoint.name,
@@ -74,13 +57,32 @@ class StationDict(Dictionary):
         self[row['id']] = row
         print_file_exists(self.write(fc_globals.STATIONS_FILE))
 
-
     def comment_waypoint(self, waypoint_id: str):
         if waypoint_id in self:
             print(f'Excluding {waypoint_id} from station dictionary')
             self['#' + waypoint_id] = self.pop(waypoint_id)
             self.write(fc_globals.STATIONS_FILE)
 
+    def get_request(self):
+        my_request = "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.xml?type=currentpredictions&units=english"
+        for _ in range(3):
+            try:
+                print(f'Requesting list of stations')
+                my_response = requests.get(my_request)
+                my_response.raise_for_status()
+                stations_tree = SoupFromXMLResponse(StringIO(my_response.content.decode())).soup
+                row_array = [{'id': station_tag.find_next('id').text,
+                    'name': station_tag.find_next('name').text, 'lat': float(station_tag.find_next('lat').text),
+                    'lon': float(station_tag.find_next('lng').text), 'type': station_tag.find_next('type').text,
+                    'folder_name': station_tag.find_next('type').text + ' ' + station_tag.find_next('id').text}
+                    for station_tag in stations_tree.find_all('Station')]
+                row_df = DataFrame(row_array).drop_duplicates()
+                row_df['folder'] = row_df['folder_name'].apply(self.absolute_path_string)
+                row_dict = row_df.to_dict('records')
+                self.update({r['id']: r for r in row_dict})
+                break
+            except requests.exceptions.RequestException:
+                sleep(1)
 
 class OneMonth(DataFrame):
 
@@ -92,7 +94,7 @@ class OneMonth(DataFrame):
         if month < 1 or month > 12:
             raise ValueError
 
-        attempts = 10
+        attempts = 20
         for attempt in range(attempts):
             try:
                 my_response = requests.get(self.url(month, year, waypoint))
@@ -108,21 +110,23 @@ class OneMonth(DataFrame):
                 # check frame
                 if frame.empty or frame.isna().all().all():
                     raise EmptyResponse(f'{exception_message} attempt: {attempt + 1}')
-                frame['Time'] = to_datetime(frame.Time, utc=True)
                 if frame['Time'].duplicated().any():
                     raise DuplicateValues(exception_message)
                 if not frame['Time'].is_monotonic_increasing:
                     raise NonMonotonic(exception_message)
 
+                frame['Time'] = to_datetime(frame.Time, utc=True)
                 frame.drop(columns=['Depth', 'Bin'], inplace=True)
                 frame['stamp'] = frame.Time.apply(dt.timestamp).astype(int)
 
                 break  # break for success
             except Exception as e:
-                if attempt < attempts and e.__class__.__name__ != 'DuplicateValues' and e.__class__.__name__ != 'NonMonotonic':
-                    sleep(1)
+                if isinstance(e, (DuplicateValues, NonMonotonic)):
+                    raise
+                if attempt < attempts - 1:
+                    sleep(min(2 ** attempt, 8))
                 else:
-                    raise type(e)(f'{exception_message}') from e
+                    raise
 
         super().__init__(data=frame)
 
@@ -157,6 +161,11 @@ class SixteenMonths(DataFrame):
             months.extend([OneMonth(m, year - 1, waypoint) for m in range(11, 13)])
             months.extend([OneMonth(m, year, waypoint) for m in range(1, 13)])
             months.extend([OneMonth(m, year + 1, waypoint) for m in range(1, 3)])
+
+            # print(f"Number of months: {len(months)}")
+            # for i, m in enumerate(months):
+            #     print(f"{waypoint.id}  Month {i}: shape={m.shape}, empty={m.empty}, all_na={m.isna().all().all()}")
+
             frame = concat(months, axis=0, ignore_index=True)
         except Exception as e:
             raise e
