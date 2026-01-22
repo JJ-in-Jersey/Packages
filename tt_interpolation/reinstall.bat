25c30fd8b81e44a8ceadbb4f291f8420ba46cc9d
albatross <hercules.class011@passinbox.com>
2026-01-21 19:52:27 -0500

    added python -m to reinstall.bat

--- tt_interpolation/reinstall.bat
+++ tt_interpolation/reinstall.bat
@@ -1,10 +1,10 @@
 python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 
-pip cache purge
+python -m pip cache purge
 cd %userprofile%/PycharmProjects/Packages
 
-pip uninstall tt_interpolation -y
-pip install ./tt_interpolation
+python -m pip uninstall tt_interpolation -y
+python -m pip install ./tt_interpolation
 
 python %userprofile%/PycharmProjects/Packages/bin/cleanup.py
 pause
